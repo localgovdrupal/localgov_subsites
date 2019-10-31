b81389079b8e5365e3055fd5888cbecdb92cf5cf
Adnan <adnan.muhammad@croydon.gov.uk>
2019-10-31 16:20:19 +0000

    Campaigns page caching: Adding cache tags.
    Every Campaigns page navigation block is dependent on the Campaign homepage and
    its child Campaign pages.  The cache tags should reflect that.

--- src/Plugin/Block/CampaignNavigationBlock.php
+++ src/Plugin/Block/CampaignNavigationBlock.php
@@ -6,7 +6,10 @@ use Drupal\bhcc_campaign\Node\CampaignMaster;
 use Drupal\bhcc_campaign\Node\CampaignSingleton;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\Cache;
+use Drupal\Core\Cache\CacheableDependencyInterface;
 use Drupal\node\Entity\Node;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+use Drupal\node\NodeInterface;
 
 /**
  * Class CampaignNavigationBlock
@@ -15,7 +18,10 @@ use Drupal\node\Entity\Node;
  *
  * @Block(
  *   id = "campaign_navigation_block",
- *   admin_label = "Campaign navigation"
+ *   admin_label = "Campaign navigation",
+ *   context_definitions = {
+ *     "node" = @ContextDefinition("entity:node")
+ *   }
  * )
  */
 class CampaignNavigationBlock extends BlockBase {
@@ -49,7 +55,7 @@ class CampaignNavigationBlock extends BlockBase {
    * @throws \Drupal\Core\TypedData\Exception\MissingDataException
    */
   private function getCampaign() {
-    $node = \Drupal::requestStack()->getCurrentRequest()->get('node');
+    $node = $this->getContextValue('node');
 
     if ($node instanceof CampaignSingleton) {
       $node = $node->getParent();
@@ -69,7 +75,8 @@ class CampaignNavigationBlock extends BlockBase {
   private function formatLinks(CampaignMaster $campaign) {
 
     // Get current node and store nid as variable currentNid
-    $currentNode = \Drupal::routeMatch()->getParameter('node');
+    $currentNode = $this->getContextValue('node');
+
     if ($currentNode instanceof \Drupal\node\NodeInterface) {
       $currentNid = $currentNode->id();
     }
@@ -113,8 +120,6 @@ class CampaignNavigationBlock extends BlockBase {
           'url' => $node->toUrl()
         ];        
       }
-
-
     }
 
     return $links;
@@ -124,6 +129,64 @@ class CampaignNavigationBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function getCacheContexts() {
-    return Cache::mergeContexts(parent::getCacheContexts(), array('route'));
+    return Cache::mergeContexts(parent::getCacheContexts(), ['route']);
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+
+    $homepage = $this->getCampaign();
+    $campaign_nodes = $this->listHomepageAndPages($homepage);
+    $campaign_cache_tags = $this->prepareCacheTagsForCampaign($campaign_nodes);
+
+    return Cache::mergeTags(parent::getCacheTags(), $campaign_cache_tags);
+  }
+
+  /**
+   * All cache tags for *a* Campaign.
+   *
+   * List cache tags for the given Campaign homepage and its child Campaign
+   * pages.
+   *
+   * @param array $campaign_nodes
+   *   List of nodes.
+   *
+   * @return array
+   *   List of strings.
+   */
+  protected function prepareCacheTagsForCampaign(array $campaign_nodes): array {
+
+    $list_of_tag_collections = array_map(function (CacheableDependencyInterface $cacheable): array {
+      return $cacheable->getCacheTags();
+    }, $campaign_nodes);
+
+    $merged_tags = array_reduce($list_of_tag_collections, [Cache::class, 'mergeTags'], $initial = []);
+    return $merged_tags;
+  }
+
+  /**
+   * List of Campaign homepage and its child Campaign pages.
+   *
+   * @return array
+   *   List of nodes.
+   */
+  protected function listHomepageAndPages(NodeInterface $homepage): array {
+
+    // Weed out the references to deleted nodes.
+    $existing_pages = array_filter(iterator_to_array($homepage->field_campaign_pages), function (EntityReferenceItem $ref): bool {
+      return isset($ref->entity);
+    });
+
+    $existing_page_nodes = array_map(function (EntityReferenceItem $ref) {
+      return $ref->entity;
+    }, $existing_pages);
+
+    $related_campaign_nodes = $existing_page_nodes;
+    $related_campaign_nodes[] = $homepage;
+
+    return $related_campaign_nodes;
+  }
+
 }
