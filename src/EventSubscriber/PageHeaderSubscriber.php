523074643750688d9c2f6f353099da3669283969
Stephen Cox <stephen-cox@users.noreply.github.com>
2020-07-22 08:14:00 +0100

    Added event subscriber to hide page header block. localgovdrupal/localgov/issues/87 (#27)

---
+++ src/EventSubscriber/PageHeaderSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\localgov_campaigns\EventSubscriber;
+
+use Drupal\node\Entity\Node;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\localgov_core\Event\PageHeaderDisplayEvent;
+
+/**
+ * Class PageHeaderSubscriber.
+ *
+ * @package Drupal\localgov_campaigns\EventSubscriber
+ */
+class PageHeaderSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      PageHeaderDisplayEvent::EVENT_NAME => ['setPageHeader', 0],
+    ];
+  }
+
+  /**
+   * Hide page header block.
+   */
+  public function setPageHeader(PageHeaderDisplayEvent $event) {
+    if ($event->getEntity() instanceof Node &&
+      ($event->getEntity()->bundle() == 'localgov_campaigns_overview' ||
+      $event->getEntity()->bundle() == 'localgov_campaigns_page')
+    ) {
+      $event->setVisibility(FALSE);
+    }
+  }
+
+}
