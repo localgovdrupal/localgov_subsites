9c9cc1809b391527cbcea0fde4107a79924dd5c8
Andy Broomfield <andy.broomfield@brighton-hove.gov.uk>
2023-07-12 11:31:13 +0100

    Fix PHP error on subsite page when overview is unpublished
    Fix #118
    
    Set the $subsite_id and $overview_entity to NULL if the overview is
    unpublished and the user doesn't have access to it.
    (It's not found in loadAndAccessCheckEntitysForTreeNodes).
    This also required a change in the subsite navigation twig to check that the
    link has a url before attempting to output it, a simmilar change will be
    needed on any theme that has implemented the template.
    
    Note: The test fails here due to the way the blocks cache.

--- src/Plugin/Block/SubsitesNavigationBlock.php
+++ src/Plugin/Block/SubsitesNavigationBlock.php
@@ -7,6 +7,7 @@ use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Template\Attribute;
+use Drupal\Node\NodeInterface;
 
 /**
  * Class SubsiteNavigationBlock.
@@ -56,8 +57,17 @@ class SubsitesNavigationBlock extends SubsitesAbstractBlockBase {
       $mapper = \Drupal::service('entity_hierarchy.entity_tree_node_mapper');
       $entities = $mapper->loadAndAccessCheckEntitysForTreeNodes('node', $tree, $cache);
       $items = $this->nestTree($tree, $ancestors, $entities);
-      $subsite_id = $entities[$ancestors[0]]->id();
-      $overview_entity = $entities[$ancestors[0]];
+
+      // Sometimes the subsite is missing if the overview is unpublished and
+      // the user is anonymous, in which case set it to NULL.
+      if (!empty($entities[$ancestors[0]]) && $entities[$ancestors[0]] instanceof NodeInterface) {
+        $subsite_id = $entities[$ancestors[0]]->id();
+        $overview_entity = $entities[$ancestors[0]];
+      }
+      else {
+        $subsite_id = NULL;
+        $overview_entity = NULL;
+      }
     }
     elseif ($subsite_entity->bundle('localgov_subsites_overview')) {
       // Subsite overview page with no children.
